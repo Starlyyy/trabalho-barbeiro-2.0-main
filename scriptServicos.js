9bd67e427cb29f47d1e6d00c459777782beb8383
Starlyyy <izabella.costa.tds2023@gmail.com>
2024-12-14 22:23:24 -0300

    nAguentoMais

---
+++ scriptServicos.js
@@ -0,0 +1,18 @@
+let nome = localStorage.getItem('nomeUsuario');
+document.getElementById('usuario').textContent = "Olá, " + nome + "! Bem-vindo(a) de volta.";
+
+function clickMenu(){
+    if (itens.style.display == "block") {
+        itens.style.display = "none";
+    } else {
+        itens.style.display = "block";
+    }
+}
+
+// document.querySelector('#lista').addEventListener('click', mostrarMenu());
+
+// const imagens = ['','','','']
+
+// function alternarFoto(){
+
+// }
